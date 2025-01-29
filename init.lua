2498d2370133438ab8d1b65b041cc143398a49ba
Matej Bruck <matej.bruck@gmail.com>
2025-01-29 22:56:21 +0100

    Initial commit

--- init.lua
+++ init.lua
@@ -10,6 +10,8 @@ if not vim.uv.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+vim.opt.termguicolors = true
+
 
 local lazy_config = require "configs.lazy"
 
@@ -24,6 +26,36 @@ require("lazy").setup({
 
   { import = "plugins" },
 }, lazy_config)
+
+
+
+
+require("transparent").setup({
+  -- table: default groups
+  groups = {
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+    'EndOfBuffer',
+  },
+  -- table: additional groups that should be cleared
+  extra_groups = {
+    "NormalFloat",
+    "Normal",
+    "NvimTreeNormal",
+    "NvimTreeNormalFloat",
+ 	  "NvimTreeNormalNC",
+  },
+  -- table: groups you don't want to clear
+  exclude_groups = {},
+  -- function: code to be executed after highlight groups are cleared
+  -- Also the user event "TransparentClear" will be triggered
+  on_clear = function() end,
+})
+
+
+
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,19 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "xiyaowong/transparent.nvim"
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+            "vim", "lua", "vimdoc",
+            "html", "css", "cpp", "c",
+            "python","cmake", "bash",
+            "awk", "doxygen"
+   		},
+    },
+  },
 }
